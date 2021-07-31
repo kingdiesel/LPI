37cb9956282602d24a0054eca7947a8b12e71ba0
Aleksey Spivak <aspivak@gmail.com>
2021-07-31 13:15:49 -0500

    test various grammars

--- LPITest/Lexer/TestLexer.cpp
+++ LPITest/Lexer/TestLexer.cpp
@@ -5,7 +5,18 @@
 
 namespace TestLexerStrings
 {
-	const std::string grammar = "; basic adventure game grammar, <command> must always be present, it's the expected root\n"
+	const std::string basic_grammar = "; basic adventure game grammar, <command> must always be present, it's the expected root\n"
+		"<command> ::= LOOK | HELP\n";
+
+	const std::string intermediate_grammar = "; basic adventure game grammar, <command> must always be present, it's the expected root\n"
+		"<command> ::= <unary_verb> | <verb> <noun> | <verb> <adjective> <noun> | <verb> <adjective> <noun> <preposition> <adjective> <noun>\n"
+		"<unary_verb> ::= LOOK | HELP\n"
+		"<verb> ::= PICKUP | INSERT | GIVE | WALK | ASK\n"
+		"<adjective> ::= BLUE | RUSTY | THIRSTY | EMPTY | OLD\n"
+		"<preposition> ::= INTO | TO | ABOUT\n"
+		"<noun> ::= NORTH | SOUTH | EAST | WEST | FROG | LLAMA | GOAT | KEY | LOCK | WATERFLASK\n";
+
+	const std::string advanced_grammar = "; basic adventure game grammar, <command> must always be present, it's the expected root\n"
 		"<command> ::= <unary_verb> | <verb> <noun> | <verb> <adjective> <noun> | <verb> <adjective> <noun> <preposition> <adjective> <noun>\n"
 		"<unary_verb> ::= LOOK | HELP\n"
 		"<verb> ::= PICKUP | INSERT | GIVE | WALK | ASK\n"
@@ -17,9 +28,22 @@ namespace TestLexerStrings
 		"<noun_object> ::= KEY | LOCK | WATERFLASK\n";
 }
 
-TEST(TestLexer, TestMatch)
+TEST(TestLexer, TestBasicGrammarParsing)
 {
 	BNF grammar;
-	const bool parsed_grammar = grammar.ParseString(TestLexerStrings::grammar);
+	const bool parsed_grammar = grammar.ParseString(TestLexerStrings::basic_grammar);
+	EXPECT_TRUE(parsed_grammar);
+}
+
+TEST(TestLexer, TestIntermediateGrammarParsing)
+{
+	BNF grammar;
+	const bool parsed_grammar = grammar.ParseString(TestLexerStrings::intermediate_grammar);
+	EXPECT_TRUE(parsed_grammar);
+}
+TEST(TestLexer, TestAdvancedGrammarParsing)
+{
+	BNF grammar;
+	const bool parsed_grammar = grammar.ParseString(TestLexerStrings::advanced_grammar);
 	EXPECT_TRUE(parsed_grammar);
 }
